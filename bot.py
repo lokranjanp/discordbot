2a14eaf40113e12332006c2460ac657672fcf583
lokranjanp <lokranjan03@gmail.com>
2024-08-16 11:00:23 +0530

    setup venv

---
+++ bot.py
@@ -0,0 +1,12 @@
+from os import environ
+import discord
+from discord.ext import commands
+from discord.ext.commands.cooldowns import BucketType
+from discord.ext.commands import Bot
+import random
+# from commands.main import *
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+import re
+import html
+
